427e4f756468fd4e81c545b69206547d0f87717f
polyphant1 <chrisl1991@hotmail.co.uk>
2016-10-22 16:53:44 +0200

    finished intro tutorial

--- arxview/polls/models.py
+++ arxview/polls/models.py
@@ -1,27 +1,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 from django.utils import timezone
 
 import datetime
-#from django.utils.encoding import python_2_unicode_compatible
 
 
-#@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date() <= timezone.now()
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
-#@python_2_unicode_compatible  # only if you need to support Python 2
+
 class Choice(models.Model):
 
     def __str__(self):
